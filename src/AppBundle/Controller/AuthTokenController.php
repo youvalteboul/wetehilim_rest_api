68c00baad1cdf29cd0915955681a73d6b5c25ef1
Youval Teboul <youval.teboul@gmail.com>
2016-11-20 01:48:39 +0100

    delete use JsonResponse (not used)

--- src/AppBundle/Controller/AuthTokenController.php
+++ src/AppBundle/Controller/AuthTokenController.php
@@ -2,7 +2,6 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest; // alias pour toutes les annotations
